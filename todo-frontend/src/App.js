290a9d5a0cffd541d4811468672b3ade179bcf04
Kaique Bernardo <kaiquebernardo02@gmail.com>
2021-08-04 01:52:41 -0300

    create-react-app

---
+++ todo-frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './todo/todo'
+import About from './about/about'
+
+function App() {
+  return (
+    <div className="container">
+      <Todo />
+      <About />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ todo-frontend/src/todo/todo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const ToDo = () => {
+
+  return (
+    <div>
+      <h1>To Do</h1>
+    </div>
+  )
+}
+
+export default ToDo
