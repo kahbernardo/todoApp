74a90b100739085bad613c59c803f4320507c8da
Kaique Bernardo <kaiquebernardo02@gmail.com>
2021-08-15 17:24:28 -0300

    correcao referencias

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Todo from './todo/todo'
-import About from './about/about'
+import Todo from './views/todo'
+import About from './views/about'
 
 function App() {
   return (
