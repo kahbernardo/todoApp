f19a63b29e41ea311e1ea8be314c5f593b0e9af0
Kaique Bernardo <kaiquebernardo02@gmail.com>
2021-08-19 00:42:06 -0300

    configuracao router

--- todo-frontend/src/components/sideMenu/index.jsx
+++ todo-frontend/src/components/sideMenu/index.jsx
@@ -2,25 +2,31 @@ import React from 'react'
 import * as ST from './styles'
 import Button from '../button'
 import icons from '../../assets/images/icons'
+import { Link } from 'react-router-dom'
 
 const sideMenu = () => {
-  console.log(333, icons)
 
   return (
     <div>
       <ST.MenuContainer >
         <ST.MenuMainButton >
-          <Button
-            icon={icons.menu}
-          />
+          <Link to='/todo'>
+            <Button
+              icon={icons.menu}
+            />
+          </Link>
         </ST.MenuMainButton >
         <ST.MenuCustomButtons >
-          <Button
-            icon={icons.bell}
-          />
-          <Button
-            icon={icons.mail}
-          />
+          <Link to='/todo'>
+            <Button
+              icon={icons.bell}
+            />
+          </Link>
+          <Link to='/about'>
+            <Button
+              icon={icons.mail}
+            />
+          </Link>
         </ST.MenuCustomButtons >
       </ST.MenuContainer >
     </div>
