b3372be33494617de8151759b1cf96ff656d3e6a
Kaique Bernardo <kaiquebernardo02@gmail.com>
2021-08-27 01:27:42 -0300

    default input

--- todo-frontend/src/components/input/index.jsx
+++ todo-frontend/src/components/input/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import * as ST from './styles'
 
-const Input = ({ change, placeholder, title }) => {
+const Input = ({ change, placeholder, title, largura, altura, mascara, value }) => {
+
+  const onChange = (content) => {
+    if (content == value) return
+    change && change(content)
+  }
 
   return (
-    <ST.InputContainer>
+    <ST.InputContainer
+      largura={largura}
+      altura={altura}
+    >
       <ST.InputTitle>
         {title}
       </ST.InputTitle>
       <ST.Input
-        onChange={change}
+        onChange={onChange}
+        value={mascara ? mascara(value) : value}
         placeholder={placeholder}
       />
     </ST.InputContainer>
